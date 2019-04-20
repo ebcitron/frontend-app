bec5607a598912d5b7c19d3dbb654460bad25dd3
theryanclausen <theryanclausen@gmail.com>
2019-04-20 09:59:42 -0700

    cra init, stripped and scaffolded

---
+++ blackhole/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Blackhole
+      </div>
+    );
+  }
+}
+
+export default App;
