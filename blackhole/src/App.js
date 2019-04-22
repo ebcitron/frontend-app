08200aba8396537262fa7ec953d54f038516ef8a
Eli Citron <ebcitron@gmail.com>
2019-04-22 13:52:36 -0700

    Designing things

--- blackhole/src/App.js
+++ blackhole/src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
+// import React from 'react';
+// import {connect } from 'react-redux';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Blackhole
-      </div>
-    );
-  }
-}
+// class App extends React.Component {
+// // CDM - check if user has token, if the token is in localstorage, login using this.props.login(JSON.parse(localStorage.getItem('username')));
 
-export default App;
+// //Display Nav bar with Username, hole/home, post, logout if logged in
+// //if logged out display nav bar with register, login, options.
+
+// //Render out the app, if user is logged in, display an internal App Home component with all of the logged in functionality,
+// //Else display the Registration/login page
+
+
+
+// const mapStateToProps = state => {
+// return {
+// username: this.state.login.username,
+// fetchingPosts: this.state.login.fetchingPosts,
+// postsFetched: this.state.login.postsFetched,
+// isLogingIn: this.state.login.isLogingIn,
+// isLoggedIn: this.state.login.isLoggedIn,
+
+
+// };
+
+
+// };
+
+// export default connect( mapStateToProps, {homepage, login, logout})(App);
